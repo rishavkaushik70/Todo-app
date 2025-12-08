ed93f77892fe3265cb6f75e838208f9b3cce98ef
rishavkaushik70 <rishavkaushik70@gmail.com>
2025-12-08 12:41:04 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,53 @@
+const inputBox = document.getElementById("input-box")
+const listContainer = document.getElementById("list-container")
+const button = document.querySelector(".btn")
+
+
+button.addEventListener("click", (e)=>{
+    if(inputBox.value === ''){
+        alert("you must write something !!")
+    }else{
+        const li = document.createElement("li")
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        const span = document.createElement("span")
+        span.innerHTML = "\u00d7";
+        li.appendChild(span)    
+    }
+    inputBox.value = "";
+    saveData()
+}  )
+   
+listContainer.addEventListener("click", (e)=>{
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData()
+
+    }else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove()
+        saveData()
+    }
+}, false);
+
+inputBox.addEventListener("keypress", function(e){
+    if(e.key === "Enter"){
+        button.click();
+    }
+});
+
+
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+
+showTask();
+
+
+
+
+
+
